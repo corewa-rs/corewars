c4fa656a56b042843824f600a4b4745f1bfcbf1f
Ian Chamberlain <ian.h.chamberlain@gmail.com>
2020-06-21 16:09:05 -0400

    Add test cases for labels and fix logic errors

--- corewa_rs/src/phased_parser/phase/expand.rs
+++ corewa_rs/src/phased_parser/phase/expand.rs
@@ -44,105 +44,56 @@ impl Info {
     /// Possible collected label declarations include:
     ///     - <lbl> EQU <text>
     ///     -       EQU <text>
+    ///     - <lbl> [instruction]
     fn collect_label_declarations(&mut self, input_lines: Vec<String>) {
-        use LabelValue::*;
-
-        let mut current_equ: Option<(String, Vec<String>)> = None;
-        let mut pending_labels: HashSet<String> = HashSet::new();
+        let mut collector = LabelCollector::new();
 
         for line in input_lines.into_iter() {
             let tokenized_line: Vec<&str> = line.split_whitespace().collect();
             let first_token = tokenized_line[0].to_uppercase();
 
             if Self::is_equ(&first_token) {
-                if let Some((_, ref mut values)) = &mut current_equ {
-                    values.push(tokenized_line[1..].join(" "));
-                } else {
-                    // TODO (#25) Syntax error, first occurrence of EQU without label
-                }
+                collector.process_equ_continuation(&tokenized_line);
                 continue;
             }
 
             if Opcode::from_str(&first_token).is_ok() {
+                self.labels
+                    .extend(collector.resolve_pending_labels(self.lines.len()));
                 self.lines.push(line);
-
-                for pending_label in pending_labels.into_iter() {
-                    self.labels.insert(pending_label, Offset(self.lines.len()));
-                }
-                pending_labels = HashSet::new();
-
-                if let Some((multiline_equ_label, values)) = current_equ {
-                    // Reached the last line in an equ, add to table and reset
-                    self.labels
-                        .insert(multiline_equ_label, Substitution(values));
-                    current_equ = None;
-                }
                 continue;
             }
 
             // At this point, treat first_token as a label since it was not a keyword
-
-            if self.labels.contains_key(&first_token) {
-                // TODO substitutions, warnings, etc.
+            if self.labels.contains_key(&first_token)
+                || collector.pending_labels.contains(&first_token)
+            {
+                // TODO substitutions?
+                // Definitely want warnings for duplicate label decl
                 continue;
             }
 
             if let Some(second_token) = tokenized_line.get(1) {
                 if Self::is_equ(second_token) {
-                    assert!(
-                        current_equ.is_none(),
-                        "Already parsing EQU: {:?} but encountered more in line: {:?}",
-                        &current_equ,
-                        &tokenized_line,
-                    );
-
-                    if tokenized_line.len() <= 2 {
-                        // TODO: warning empty RHS of EQU (see docs/pmars-redcode-94.txt:170)
-                    }
-
-                    let substitution = tokenized_line
-                        .get(2..)
-                        .filter(|s| !s.is_empty())
-                        .map(|tokens| vec![tokens.join(" ")])
-                        .unwrap_or_default();
-
-                    current_equ = Some((tokenized_line[0].to_owned(), substitution));
+                    collector.process_equ(&tokenized_line)
                 } else {
                     // label on top of normal line
-                    // TODO dedupe with `Opcode::from_str(&first_token).is_ok()` above
+                    collector
+                        .pending_labels
+                        .insert(tokenized_line[0].to_string());
+                    self.labels
+                        .extend(collector.resolve_pending_labels(self.lines.len()));
                     self.lines.push(tokenized_line[1..].join(" "));
-
-                    pending_labels.insert(first_token);
-                    for pending_label in pending_labels.into_iter() {
-                        self.labels.insert(pending_label, Offset(self.lines.len()));
-                    }
-                    pending_labels = HashSet::new();
-
-                    if let Some((multiline_equ_label, values)) = current_equ {
-                        // Reached the last line in an equ, add to table and reset
-                        self.labels
-                            .insert(multiline_equ_label, Substitution(values));
-                        current_equ = None;
-                    }
                 }
             } else {
                 // Label declaration by itself on a line
-                pending_labels.insert(first_token);
-                continue;
+                collector
+                    .pending_labels
+                    .insert(tokenized_line[0].to_string());
             }
         }
 
-        // TODO dedupe with `Opcode::from_str(&first_token).is_ok()` above
-
-        if !pending_labels.is_empty() {
-            // TODO warning for empty definition for each pending label
-        }
-
-        if let Some((multiline_equ_label, values)) = current_equ {
-            // Reached EOF while in EQU, add to table
-            self.labels
-                .insert(multiline_equ_label, Substitution(values));
-        }
+        self.labels.extend(collector.finish());
     }
 
     fn is_equ(token: &str) -> bool {
@@ -150,6 +101,79 @@ impl Info {
             PseudoOpcode::from_str(&token.to_uppercase()),
             Ok(PseudoOpcode::Equ)
         )
+    }
+}
+
+#[derive(Debug)]
+struct LabelCollector {
+    pub current_equ: Option<(String, Vec<String>)>,
+    pub pending_labels: HashSet<String>,
+}
+
+impl LabelCollector {
+    fn new() -> Self {
+        Self {
+            current_equ: None,
+            pending_labels: HashSet::new(),
+        }
+    }
+
+    pub fn process_equ(&mut self, tokenized_line: &[&str]) {
+        assert!(
+            self.current_equ.is_none(),
+            "Already parsing EQU: {:?} but encountered more in line: {:?}",
+            &self.current_equ,
+            &tokenized_line,
+        );
+
+        if tokenized_line.len() <= 2 {
+            // TODO: warning empty RHS of EQU (see docs/pmars-redcode-94.txt:170)
+        }
+
+        let substitution = tokenized_line
+            .get(2..)
+            .filter(|s| !s.is_empty())
+            .map(|tokens| vec![tokens.join(" ")])
+            .unwrap_or_default();
+
+        self.current_equ = Some((tokenized_line[0].to_owned(), substitution));
+    }
+
+    pub fn process_equ_continuation(&mut self, tokenized_line: &[&str]) {
+        if let Some((_, ref mut values)) = self.current_equ {
+            values.push(tokenized_line[1..].join(" "));
+        } else {
+            // TODO (#25) Syntax error, first occurrence of EQU without label
+        }
+    }
+
+    pub fn resolve_pending_labels(
+        &mut self,
+        offset: usize,
+    ) -> impl Iterator<Item = (String, LabelValue)> {
+        let mut result = HashMap::new();
+
+        let pending_labels = std::mem::replace(&mut self.pending_labels, HashSet::new());
+        for pending_label in pending_labels.into_iter() {
+            result.insert(pending_label, LabelValue::Offset(offset));
+        }
+
+        let current_equ = self.current_equ.take();
+        if let Some((multiline_equ_label, values)) = current_equ {
+            // Reached the last line in an equ, add to table and reset
+            result.insert(multiline_equ_label, LabelValue::Substitution(values));
+        }
+
+        result.into_iter()
+    }
+
+    pub fn finish(self) -> Option<(String, LabelValue)> {
+        if !self.pending_labels.is_empty() {
+            // TODO warning for empty definition for each pending label
+        }
+
+        self.current_equ
+            .map(|(label, values)| (label, LabelValue::Substitution(values)))
     }
 }
 
@@ -176,7 +200,7 @@ mod tests {
     }
 
     #[test]
-    fn collect_multiline_equ() {
+    fn collect_multi_line_equ() {
         let lines = vec![
             String::from("foo equ mov 1, 1"),
             String::from("equ jne 0, -1"),
@@ -195,5 +219,47 @@ mod tests {
             }
         );
         assert_eq!(info.lines, vec![String::from("add 2, 3")]);
+    }
+
+    #[test]
+    fn collect_label_offset() {
+        let lines = vec![String::from("foo mov 1, 1")];
+
+        let info = Info::collect(lines);
+
+        assert_eq!(
+            info.labels,
+            hashmap! {
+                String::from("foo") => Offset(0),
+            }
+        );
+        assert_eq!(info.lines, vec![String::from("mov 1, 1")]);
+    }
+
+    #[test]
+    fn collect_multi_label_offsets() {
+        let lines = vec![
+            String::from("start"),
+            String::from("_alias mov 1, 1"),
+            String::from("other_alias"),
+            String::from("other_alias2"),
+            String::from("MOV 0, 1"),
+        ];
+
+        let info = Info::collect(lines);
+
+        assert_eq!(
+            info.labels,
+            hashmap! {
+                String::from("start") => Offset(0),
+                String::from("_alias") => Offset(0),
+                String::from("other_alias") => Offset(1),
+                String::from("other_alias2") => Offset(1),
+            }
+        );
+        assert_eq!(
+            info.lines,
+            vec![String::from("mov 1, 1"), String::from("MOV 0, 1")]
+        );
     }
 }
